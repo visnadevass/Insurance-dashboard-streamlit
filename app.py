340778207f58247b220b2d7800a9dd5f6b00cca6
visnadevass <visna.20241129@iit.ac.lk>
2026-07-12 23:03:51 +0530

    Created basic Streamlit application

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Insurance Website Analytics Dashboard",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 Insurance Website Analytics Dashboard")
+
+st.write("Welcome to the Insurance Website Analytics Dashboard!")
